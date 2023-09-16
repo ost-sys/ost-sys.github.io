204c1c1ac107c8d6e709624532b1a9606f971760
Operating System Tester <66862161+ost-sys@users.noreply.github.com>
2023-09-16 22:10:51 +0300

    Restarted the timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("sep 16, 2023 00:00:00").getTime();
+var countDownDate = new Date("sep 16, 2024 06:50:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
