6729dd7bcc814bfe6116de98deb1cd4022def426
Operating System Tester <gadji.pirov@gmail.com>
2024-11-11 20:34:14 +0300

    Error

--- assets/js/btn-cp.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-    var copyText = document.getElementById("myInput");
-    copyText.select();
-    document.execCommand("copy");
-    alert("Скопировано!");
-}
--- assets/js/clock.js
+++
@@ -1,5 +0,0 @@
-setInterval(function () {
-    var now = new Date();
-    var clock = document.getElementById("clock");
-    clock.innerHTML = now.toLocaleTimeString();
-}, 1000);
